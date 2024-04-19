ad2e31336f02b689db74ec61abebd9ae09ec608b
Giorgio Messore <giorgio.messore@live.it>
2024-04-20 00:10:47 +0200

    toolbar updated

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,8 +1,14 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.css'
+  styleUrl: './toolbar.component.css',
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  closeOffcanvas() {
+    document
+      .querySelector('.offcanvas')
+      ?.setAttribute('data-bs-dismiss', 'offcanvas');
+  }
+}
