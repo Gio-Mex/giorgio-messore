5216b17ac53c1dbc181bf4c9ca06f350f11b7639
Giorgio Messore <giorgio.messore@live.it>
2025-11-16 13:00:52 +0100

    hero update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -45,7 +45,7 @@ onMounted(() => {
       class="relative h-[300px] md:h-[600px] lg:h-[400px] xl:h-[700px] w-2/3 md:w-full lg:w-1/3 mt-10 ms-auto rounded-es-full rounded-s-full shadow-md overflow-hidden hover:scale-105 hover:shadow-blue-500/50 transition-transform duration-500"
     >
       <img
-        class="absolute top-5 left-2 lg:left-0 h-[300px] md:h-[600px] lg:h-[400px] xl:h-[700px] object-cover z-10"
+        class="absolute lg:left-0 h-[300px] md:h-[600px] lg:h-[400px] xl:h-[700px] object-cover z-10"
         :src="myPic"
         alt="developer"
         loading="eager"
